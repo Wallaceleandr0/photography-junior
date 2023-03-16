189b09551b438f3015e86bb2c94033138a6519b2
Wallace <wallaceleandro2003@gmail.com>
2023-03-16 16:28:07 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,28 @@
+function abrirMenu () {
+    document.querySelector('.menu-hamburguer').style.display = 'flex'
+    document.querySelector('#ico-menu').style.display = 'none'
+    document.querySelector('#ico-fechar').style.display = 'block'
+    // document.querySelector('#x').style.display= 'block'
+}
+
+function fecharMenu () {
+    document.querySelector('.menu-hamburguer').style.display = 'none'
+    document.querySelector('#ico-menu').style.display = 'block'
+    document.querySelector('#ico-fechar').style.display = 'none'
+}
+
+
+
+
+
+// function abrirMenu () {
+//     document.querySelector('.menu-mobile').style.display = 'block'
+//     document.querySelector('#menu-icon').style.display = 'none'
+//     document.querySelector('#x').style.display= 'block'
+// }
+
+// function fecharMenu () {
+//     document.querySelector('.menu-mobile').style.display = 'none'
+//     document.querySelector('#menu-icon').style.display = 'block'
+//     document.querySelector('#x').style.display= 'none'
+// }
