ece3d08f896e14271a9bb850043aa6a9141acc22
Wallace <wallaceleandro2003@gmail.com>
2023-03-21 21:32:05 -0300

    presets atualizado comprar pack

--- main.js
+++ main.js
@@ -13,24 +13,3 @@ function fecharMenu () {
     document.querySelector('#ico-fechar').style.display = 'none'
 }
 
-document.querySelector('#range').addEventListener('input', e => {
-    changeWidth(e.target.value)
-});
-
-function changeWidth(value) {
-    document.querySelector('#dv-effect').style.width = `${value}%`;
-}
-
-
-
-// function abrirMenu () {
-//     document.querySelector('.menu-mobile').style.display = 'block'
-//     document.querySelector('#menu-icon').style.display = 'none'
-//     document.querySelector('#x').style.display= 'block'
-// }
-
-// function fecharMenu () {
-//     document.querySelector('.menu-mobile').style.display = 'none'
-//     document.querySelector('#menu-icon').style.display = 'block'
-//     document.querySelector('#x').style.display= 'none'
-// }
